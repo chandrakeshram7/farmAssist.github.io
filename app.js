b43008f2af7831dff8594aa9c789d7798045b603
Chandrakesh Ram <chandrakeshram31@gmail.com>
2024-02-12 11:09:27 +0530

    Fixing error

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ passport.serializeUser((user, done) => {
 //Foe Image Uploading and Storage Settings - Using Multer
 const multer = require('multer');
 let Storage = multer.diskStorage({
-    destination:'./views/public/images/',
+    destination:'./public/images/',
     filename: (req,file, cb)=>{
         // cb(null, Date.now(+file.originalname))
         cb(null, file.originalname)
