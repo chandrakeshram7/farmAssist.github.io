52cdb2d392d86fa913b79d614ca937b3fd14eb61
Chandrakesh Ram <chandrakeshram31@gmail.com>
2024-02-12 11:37:36 +0530

    Added some more fixes

--- app.js
+++ app.js
@@ -165,15 +165,15 @@ passport.serializeUser((user, done) => {
 
 //Foe Image Uploading and Storage Settings - Using Multer
 const multer = require('multer');
-let Storage = multer.diskStorage({
-    destination:'./user_photos/images/',
-    filename: (req,file, cb)=>{
-        // cb(null, Date.now(+file.originalname))
-        cb(null, file.originalname)
-    }
-})
+// let Storage = multer.diskStorage({
+//     destination:'./user_photos/images/',
+//     filename: (req,file, cb)=>{
+//         // cb(null, Date.now(+file.originalname))
+//         cb(null, file.originalname)
+//     }
+// })
 
-
+const Storage = multer.memoryStorage();
 let upload = multer({
     storage: Storage
 })
